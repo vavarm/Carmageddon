f31b8209088c38d4b71976042c30aef200eaf601
vavarm <v.racaud-minuzzi@orange.fr>
2024-03-25 22:16:14 +0100

    test: sout information in test

--- src/test/java/org/acme/CompilerTest.java
+++ src/test/java/org/acme/CompilerTest.java
@@ -92,8 +92,8 @@ class CompilerTest {
         List<String> lines = Files.readAllLines(calculationFile);
         String router = "  public static int calculate(String method) {    switch (method) {      case \"file1\":        return file1();      case \"file2\":        return file2();      default:        return 0;    }  }".replace("\n", "");
         String calculationFileContents = String.join("", lines);
-        Log.info(calculationFileContents.replace("\n", ""));
-        Log.info(router);
+        System.out.println(calculationFileContents.replace("\n", ""));
+        System.out.println(router);
         assertTrue(calculationFileContents.replace("\n", "").contains(router));
     }
 
